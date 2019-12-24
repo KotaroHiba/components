c3b67d9e689666e1fbb369a9ccb1f836df410cf6
KotaroSW <52319698+KotaroSW@users.noreply.github.com>
2019-12-25 01:00:06 +0300

    JS функционал slider готов

--- components/items/slider/slider.js
+++ components/items/slider/slider.js
@@ -1,9 +1,31 @@
-let slide = $('.slider-item');
-let calcSlide = 0;
+
 
 
 $('.slider__next').click(function () {
-   let t =  $(this).siblings('.slider-item');
-    console.log(t)
+    let thisSlider = $(this).siblings('.slider-item.active');
+
+    if ($(this).siblings('.slider-item:last').hasClass('active')) {
+        $(this).siblings('.slider-item:first').addClass('active')
+    }
+    thisSlider.removeClass('active');
+    thisSlider.next('.slider-item').addClass('active');
+
+
 });
 
+
+$('.slider__prev').click(function () {
+    let thisSlider = $(this).siblings('.slider-item.active');
+
+    if ($(this).siblings('.slider-item:first').hasClass('active')) {
+        $(this).siblings('.slider-item:last').addClass('active')
+    }
+    thisSlider.removeClass('active');
+    thisSlider.prev('.slider-item').addClass('active');
+
+
+});
+
+
+// $(this).removeClass('active');
+// $(this).next().addClass('active');
