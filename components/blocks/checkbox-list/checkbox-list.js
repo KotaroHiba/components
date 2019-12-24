16b15d5c9321dc8bec86ddb6399d20685c613f7b
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2019-12-24 18:26:37 +0300

    Finished checkbox-list

--- components/blocks/checkbox-list/checkbox-list.js
+++ components/blocks/checkbox-list/checkbox-list.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $(".checkbox-list__title").next().slideToggle();
 
+    // Функция для складки\разворачивания списка.
     $(".checkbox-list__title").click(function() {
         $(this).next().slideToggle();
+
+        // Получаем значение стрелочки и заменяем при основном действии.
         let arror = $(this).children('.checkbox-list__arror');
         if (arror.text() == 'keyboard_arrow_up') {
             arror.text('keyboard_arrow_down')
