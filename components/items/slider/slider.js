1f264ec702ee7e421713a1403fda44101a5afed6
KotaroSW <52319698+kotarosw@users.noreply.github.com>
2019-12-28 04:30:21 +0300

    Функционал слайдера сделан на 90%

--- components/items/slider/slider.js
+++ components/items/slider/slider.js
@@ -3,13 +3,18 @@
 
 $('.slider__next').click(function () {
     let thisSlider = $(this).siblings('.slider-item.active');
-
     if ($(this).siblings('.slider-item:last').hasClass('active')) {
         $(this).siblings('.slider-item:first').addClass('active')
     }
     thisSlider.removeClass('active');
     thisSlider.next('.slider-item').addClass('active');
 
+    let thisCircle = $(this).siblings('.slider__target').find('.slider__target-circle.active');
+    if ($(this).siblings('.slider__target').find('.slider__target-circle:last').hasClass('active')){
+        $(this).siblings('.slider__target').find('.slider__target-circle:first').addClass('active')
+    }
+    thisCircle.removeClass('active');
+    thisCircle.next('.slider__target-circle').addClass('active');
 
 });
 
@@ -23,9 +28,10 @@ $('.slider__prev').click(function () {
     thisSlider.removeClass('active');
     thisSlider.prev('.slider-item').addClass('active');
 
-
+    let thisCircle = $(this).siblings('.slider__target').find('.slider__target-circle.active');
+    if ($(this).siblings('.slider__target').find('.slider__target-circle:first').hasClass('active')){
+        $(this).siblings('.slider__target').find('.slider__target-circle:last').addClass('active')
+    }
+    thisCircle.removeClass('active');
+    thisCircle.prev('.slider__target-circle').addClass('active');
 });
-
-
-// $(this).removeClass('active');
-// $(this).next().addClass('active');
